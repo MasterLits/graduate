b6552ca7a836cc0f137b8b8e70e9dda556216459
Pegasov <gamma8pegasi@gmail.com>
2016-12-01 00:01:02 +0200

    Added fields fillment validation when creating a new task

--- Kaban/app/models/task.rb
+++ Kaban/app/models/task.rb
@@ -5,4 +5,5 @@ class Task < ApplicationRecord
   has_many :assigned_users, :through => :assigned_users_tasks, source: :user
   belongs_to :user
   belongs_to :category
+  validates :body, :cost, :location, presence: true
 end
