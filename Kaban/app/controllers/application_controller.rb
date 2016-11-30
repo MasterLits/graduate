8f8abb6461d0aa2d31a5497f1fbf5c543d9a944f
Pegasov <gamma8pegasi@gmail.com>
2016-11-30 02:20:42 +0200

    Added 422 error instead of CanCan::AccessDenied

--- Kaban/app/controllers/application_controller.rb
+++ Kaban/app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 
+    rescue_from CanCan::AccessDenied do |exception|
+    redirect_to '/422.html'
+    end
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :role)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :role)}
