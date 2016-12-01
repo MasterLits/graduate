f2a94237900e8f9e07675439e205e3faf96c4830
SlippyJohny <shpak.d.o@ukr.net>
2016-12-01 17:08:39 +0200

    added task search

--- Kaban/app/controllers/tasks_controller.rb
+++ Kaban/app/controllers/tasks_controller.rb
@@ -5,67 +5,71 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+    if params[:search]
+      @tasks = Task.search(params[:search]).order("created_at DESC")
+    else
+      @tasks = Task.all.order('created_at DESC')
+    end
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
 
-  # GET /tasks/new
-  def new
+    # GET /tasks/1
+    # GET /tasks/1.json
+    def show
+    end
 
-    @task = Task.new
+    # GET /tasks/new
+    def new
 
+      @task = Task.new
+    end
 
-  end
+    # GET /tasks/1/edit
+    def edit
+    end
 
-  # GET /tasks/1/edit
-  def edit
-  end
+    # POST /tasks
+    # POST /tasks.json
+    def create
 
-  # POST /tasks
-  # POST /tasks.json
-  def create
-
-    @task = current_user.tasks.create(task_params)
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to profile_path, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: current_user }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+      @task = current_user.tasks.create(task_params)
+      respond_to do |format|
+        if @task.save
+          format.html { redirect_to profile_path, notice: 'Task was successfully created.' }
+          format.json { render :show, status: :created, location: current_user }
+        else
+          format.html { render :new }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
-  def update
-    respond_to do |format|
+    # PATCH/PUT /tasks/1
+    # PATCH/PUT /tasks/1.json
+    def update
+      respond_to do |format|
 
-      if @task.update(task_params)
-        format.html { redirect_to profile_path(@task), notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        if @task.update(task_params)
+          format.html { redirect_to profile_path(@task), notice: 'Task was successfully updated.' }
+          format.json { render :show, status: :ok, location: @task }
+        else
+          format.html { render :edit }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
-  def destroy
-    @task.destroy
-    respond_to do |format|
-      format.html { redirect_to profile_path, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
+    # DELETE /tasks/1
+    # DELETE /tasks/1.json
+    def destroy
+      @task.destroy
+      respond_to do |format|
+        format.html { redirect_to profile_path, notice: 'Task was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
-  end
 
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
@@ -75,4 +79,4 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:body, :cost, :date, :location, :title, :user_id, :category_id)
     end
-end
+  end
--- Kaban/app/models/task.rb
+++ Kaban/app/models/task.rb
@@ -5,5 +5,10 @@ class Task < ApplicationRecord
   has_many :assigned_users, :through => :assigned_users_tasks, source: :user
   belongs_to :user
   belongs_to :category
-  validates :body, :cost, :location, presence: true
+  validates :body, :cost, :location, :title, :category, presence: true
+
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
+
 end
