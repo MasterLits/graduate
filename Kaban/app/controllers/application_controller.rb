3027cce5bddcdf4692368aa2fc5322bcd90be35a
SlippyJohny <shpak.d.o@ukr.net>
2016-12-01 14:48:41 +0200

    mergin from pegasov

--- Kaban/app/controllers/application_controller.rb
+++ Kaban/app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
+
 class ApplicationController < ActionController::Base
-
-  protect_from_forgery with: :exception
-    rescue_from CanCan::AccessDenied do |exception|
-    redirect_to '/422.html'
-end
-  include Errors::RescueError
   rescue_from CanCan::AccessDenied do |exception|
-  redirect_to '/422.html'
-   end
-
-
-    def re_redirect_to(location, status = 303)
-      response.location = location
-      response.status = status
-    end
-
+    redirect_to '/422.html'
+  end
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
@@ -22,8 +11,6 @@ end
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :role)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :role)}
   end
-
-
 
 
 end
